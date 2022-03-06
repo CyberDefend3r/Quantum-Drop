f78f31a2a0c780abc365aa9287c80d73daa11eba
trevormiller6 <Trevormiller6@gmail.com>
2022-03-05 20:40:07 -0800

    add terms of use

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
             [
                 span(
                     put_markdown(
-                        "# Quantum Drop - Anonymous File Sharing  \n  \n  Upload a file to share anonymously or enter passphrase to retrieve a file.\n  \n   ----\n  \n  The file you upload is deleted immediately and stored as an encrypted zip file. The only person that can see the file you upload is anyone you shared the passphrase with. Passphrases are generated randomly and not stored. There is no way to get it after it is displayed to you.\n  \n  IP addresses are not stored or any other data that can identify you.\n  \n  When retrieving a file with the passphrase a password for the zip file will be displayed and a link to download the encrypted zip file will appear once you close the password popup.\n  \n  ----"
+                        "# Quantum Drop - Anonymous File Sharing  \n  \n  Upload a file to share anonymously or enter passphrase to retrieve a file.\n  \n   ----\n  \n  The file you upload is deleted immediately and stored as an password protected zip file. Passphrases are generated randomly and not stored. There is no way to get it after it is displayed to you.\n  \n  **IP addresses are not stored or any other data that can identify you.**\n  \n  ----\n  When retrieving a file with the passphrase a password for the zip file will be displayed and a link to download the zip file will appear once you close the password popup.\n  "
                     ),
                     col=5,
                 )
@@ -39,26 +39,38 @@ def main():
     )
 
     with use_scope("1-3"):
-        put_input("something", label="Enter passphrase to retrieve file.")
-        put_button("submit", onclick=lambda: submit_passphrase(pin.something))
+        put_input("phrase", label="Enter passphrase to retrieve file:")
+        put_button("submit", onclick=lambda: submit_passphrase(pin.phrase))
         # put_file()
 
     with use_scope("1-2"):
-        a = file_upload("Upload File Anonymously")
+        popup(
+            "Terms of Use",
+            put_markdown(
+                "### Closing this means you agree to these rules:  \n  1) No adult content.\n  2) No illegal content.\n  3) You will not hold me liable for any files that are malicious or damaging to your computer or your life.\n  3) Files uploaded will only be stored for 3 days. However, there is no guarantee the file will be available when requested.\n  4) Any access to your file is your responsibility and I am not liable for any unintended access to your file.\n  5) There is no guarantee of privacy or anonymity. Though, every effort, to my ability, is made to maintain privacy and anonymity."
+            ),
+        )
+        a = file_upload("Upload File Anonymously - 50Mb max", max_size="50m")
         passphrase = uploader.upload(a)
         popup(
             "Use this passphrase to retrieve the file",
-            put_html(f"<h3>{passphrase}"),
+            put_markdown(f"**{passphrase}**"),
         )
 
 
 def submit_passphrase(passphrase):
-    name, password, zip_bytes = retriever.get(passphrase)
-    put_file(name=name, content=zip_bytes)
+    password, zip_bytes = retriever.get(passphrase)
+    put_file(name="quantum-drop.zip", content=zip_bytes)
     popup(
         "Password",
-        put_html(f"<h3>{password}"),
+        put_markdown(f"**{password}**"),
     )
 
 
 start_server(main, port=443, debug=True)
+popup(
+    "Terms of Use",
+    put_markdown(
+        "# By Closing this popup you agree to the following rules  \n  1) No adult content\n  2) No illegal content\n  3) You will not hold me liable for any files that are malicious or damaging to your computer or your life.\n  3) There is no guarantee the file will be available when requested\n  4) Files uploaded will only be stored for 3 days"
+    ),
+)
