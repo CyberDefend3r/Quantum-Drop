d6fc4b2f8e208a8891fe6aa09f2e0ddbddbf987c
trevormiller6 <Trevormiller6@gmail.com>
2022-03-05 21:59:16 -0800

    port

--- quantum_drop.py
+++ quantum_drop.py
@@ -68,7 +68,7 @@ def submit_passphrase(passphrase):
     cleanup.delete()
 
 
-start_server(main, port=8080, debug=True)
+start_server(main, debug=True)
 popup(
     "Terms of Use",
     put_markdown(
